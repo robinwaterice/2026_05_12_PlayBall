dbf7afa27b99b1beced20f7b63416b2e650b8d8b
Robin <robinwaterice@gmail.com>
2026-05-12 16:18:41 +0800

    打磚塊!

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
+  base: '/2026_05_12_PlayBall/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
